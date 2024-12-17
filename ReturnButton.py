1e4198a57b10752751cf4ab7358ab64cd7cf6155
Maiia25 <maya020591.mb@gmail.com>
2024-12-17 11:59:17 -0500

    UserRegistr added

---
+++ ReturnButton.py
@@ -0,0 +1,9 @@
+#def choosebook():
+ #   btn.destroy()
+  #  btn1.destroy()
+   # btn2.destroy()
+    #find_book_page(screen)
+#btn = Button(screen, text="Пошук книги", bg="blue", activebackground="green", fg="white",
+    #          activeforeground="red", font=("Courier", 30),
+   #              relief=FLAT, overrelief=GROOVE, command = choosebook) #relief - рамка кнопки
+#btn.pack(pady=20) #відступ
